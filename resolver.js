f1897d355768c7ce5dd73fcfb00e22621b07d1a7
Alan Gutierrez <alan@prettyrobots.com>
2012-06-18 08:54:11 -0400

    Implemented `require` attribute.

--- resolver.js
+++ resolver.js
@@ -1,9 +1,21 @@
 var parse = require("url").parse, path = require("path"), fs = require("fs"), xmldom = require("xmldom");
 module.exports.create = function create (base) {
-  return function resolver (url, callback) {
-    fs.readFile(path.resolve(base, url), "utf8", function (error, source) {
-      if (error) callback(error);
-      else callback(null, new (xmldom.DOMParser)().parseFromString(source));
-    });
+  return function resolver (url, mimeType, callback) {
+    var resolved = path.resolve(base, url)
+    switch (mimeType) {
+    case "text/javascript":
+      try {
+        callback(null, require(resolved));
+      } catch (error) {
+        callback(error);
+      }
+      break;
+    case "text/xml":
+      fs.readFile(resolved, "utf8", function (error, source) {
+        if (error) callback(error);
+        else callback(null, new (xmldom.DOMParser)().parseFromString(source));
+      });
+      break;
+    }
   }
 }
