3872f4aecd14d4ba2b0cdb4ba87219c52f730c06
Alan Gutierrez <alan@prettyrobots.com>
2012-11-30 09:50:26 -0500

    Deleted Modeler.

--- t/modeler/modeler.t.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-require("proof")(1, function (equal) {
-  var exports = {}, modeler = require("../../modeler").createModeler(exports), called = 0;
-  modeler.dynamic("watchers", function (broker, callback) { called++ });
-  exports._namespace.watchers.callback();
-  equal(called, 1, "constructed");
-});
