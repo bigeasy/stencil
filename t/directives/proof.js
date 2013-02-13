5ff9114c75fab24b9e72a3e104a608abccc7e736
Alan Gutierrez <alan@prettyrobots.com>
2013-02-13 04:03:41 -0500

    Rename `redux.js` to `index.js`.
    Grasshopper has become master.
    
    Closes #76.

--- t/directives/proof.js
+++ t/directives/proof.js
@@ -1,7 +1,7 @@
 var fs = require('fs'), path = require('path');
 module.exports = require('proof')(function () {
   var context =
-  { stencil: require('../../redux')
+  { stencil: require('../../index')
   , resolver: require('../../resolver')
   , compare: require('../compare')
   , fixture: function (file, callback) { fs.readFile(path.resolve(__dirname, file), 'utf8', callback) }
