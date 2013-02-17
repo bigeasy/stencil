959f74985834d766b660c05f1df853ca9ac6f77e
Alan Gutierrez <alan@prettyrobots.com>
2013-02-17 06:45:16 -0500

    Track frame.
    Instead of using a frame context object, I'm using an array that
    contains the state of the previous frame, where a frame is a descent
    within a specific template. The use of a frame object to gather the
    variables in the `rewrite` and directive handler function signatures
    caused the source to grow by 0.8k instead of the 0.2k that comes from
    adding the `frame` parameter to the `rewrite` function signature.

--- index.js
+++ index.js
@@ -220,7 +220,8 @@
   var handlers = {
     // The value directive replaces a value element with text from the current
     // context.
-    value: function (parent, page, template, directive, element, context, path, generating, rewrite, callback) {
+    value: function (parent, frames, page, template, library,
+                     directive, element, context, path, generating, callback) {
       var source = element.getAttribute("select").trim(), marker = follow(page, path);
 
       evaluate(source, context, check(callback, function (value) {
@@ -233,7 +234,8 @@
         callback();
       }));
     },
-    if: function (parent, page, template, directive, element, context, path, generating, rewrite, callback) {
+    if: function (parent, frames, page, template, library,
+                  directive, element, context, path, generating, callback) {
       var source = element.getAttribute("select").trim(), marker = follow(page, path);
 
       evaluate(source, context, check(callback, function (value) {
@@ -252,25 +254,31 @@
             fill(marker, fragment);
             generating = true;
           }
-          rewrite(directive.directives, path, generating, callback);
+          rewrite({}, frames, page, template, library, directive.directives,
+                  path, context, generating, callback);
         }
       }));
     },
-    else: function (parent, page, template, directive, element, context, path, generating, rewrite, callback) {
+    else: function (parent, frames, page, template, library,
+                    directive, element, context, path, generating, callback) {
       element.setAttribute("select", "true");
-      handlers.elseif(parent, page, template, directive, element, context, path, generating, rewrite, callback);
+      handlers.elseif(parent, frames, page, template, library,
+                      directive, element, context, path, generating, callback);
     },
-    elseif: function (parent, page, template, directive, element, context, path, generating, rewrite, callback) {
+    elseif: function (parent, frames, page, template, library,
+                      directive, element, context, path, generating, callback) {
       var marker = follow(page, path);
       if (parent.condition) {
         erase(marker.begin.nextSibling, marker.end);
         follow(page, path).markers = {};
         callback();
       } else {
-        handlers["if"](parent, page, template, directive, element, context, path, generating, rewrite, callback);
+        handlers["if"](parent, frames, page, template, library,
+                       directive, element, context, path, generating, callback);
       }
     },
-    each: function (parent, page, template, directive, element, context, path, generating, rewrite, callback) {
+    each: function (parent, frames, page, template, library,
+                    directive, element, context, path, generating, callback) {
       var source = element.getAttribute("select").trim(),
           idSource = element.getAttribute("key").trim(),
           into = element.getAttribute("into").trim(),
@@ -340,15 +348,18 @@
 
           previous = follow(page, path).end;
 
-          rewrite(directive.directives, path, generating, okay(shift));
+          rewrite({}, frames, page, template, library, directive.directives,
+                  path, context, generating, okay(shift));
         }
       }));
     },
     // **TODO**: Pass around a generating flag, so that we only evaluate this
     // bit once, not for performance, but to simplify the implementation.
-    block: function (parent, page, template, directive, element, context, path, generating, ignore, callback) {
+    block: function (parent, frames, page, template, library, directive, element,
+                     context, path, generating, callback) {
       if (generating) {
-        var prototype = follow(parent.template.page, parent.directive.path);
+        var caller = frames[0],
+            prototype = follow(caller.template.page, caller.directive.path);
         if (prototype.begin.nextSibling != prototype.end) {
           var marker = follow(page, path),
               fragment = copy(page.document, prototype.begin.nextSibling, prototype.end);
@@ -359,14 +370,15 @@
         }
         // **TODO**: Function signature is getting super ugly, the
         // `parent.library` is an awful kludge.
-        rewrite({ parent: parent, template: template, directive: directive },
-          page, parent.template, parent.directive.directives,
-          parent.library, context, path, generating, callback);
+        frames = frames.concat({ template: template, directive: directive });
+        rewrite({}, frames, page, caller.template, library, caller.directive.directives,
+                path, context, generating, callback);
       }
     }
   }
 
-  function rewrite (parent, page, template, directives, library, context, path, generating, callback) {
+  function rewrite (parent, frames, page, template, library,
+                    directives, path, context, generating, callback) {
     var okay = validator(callback);
 
     context = extend({}, context);
@@ -423,10 +435,9 @@
           break;
         default:
           var included;
-          if (directive.interpreter) directive.interpreter(parent, page, template, directive, element, context, sub, generating,
-            function (directives, path, generating, callback) {
-              rewrite({ parent: parent }, page, template, directives, library, context, path, generating, callback);
-            }, shift);
+          if (directive.interpreter)
+            directive.interpreter(parent, frames, page, template, library,
+                                  directive, element, context, sub, generating, shift);
           else if (directive.element && (included = library[directive.element.namespaceURI])) {
             var include = included.library[directive.element.localName];
             if (generating) {
@@ -438,9 +449,10 @@
               fill(marker, fragment);
             }
             sub.push(include.id);
-            rewrite({ parent: parent, template: template, directive: directive, library: library }, page, included, include.directives, library, context, sub, generating, okay(shift));
+            frames = frames.concat({ template: template, directive: directive, library: library });
+            rewrite({}, frames, page, included, library, include.directives, sub, context, generating, okay(shift));
           }
-          else rewrite({ parent: parent }, page, template, directive.directives, library, context, sub, generating, shift);
+          else rewrite({}, frames, page, template, library, directive.directives, sub, context, generating, shift);
           break;
         }
       }
@@ -630,7 +642,8 @@
     fetch(base, url, okay(rebase));
 
     function rebase (template) {
-      rewrite({}, page, template, template.directives, {}, parameters, [], false, okay(function () {
+      rewrite({}, [], page, template, {}, template.directives,
+              [], parameters, false, okay(function () {
         callback(null, page);
       }));
     }
@@ -672,7 +685,8 @@
       vivify(page, [], fragment);
 
       // Evaluate the template.
-      rewrite({}, page, template, template.directives, {}, parameters, [], true, okay(result));
+      rewrite({}, [], page, template, {}, template.directives,
+              [], parameters, true, okay(result));
 
       function result () {
         insertBefore(page.document, page.fragment);
