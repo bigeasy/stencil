cf8ccc8cc13dc9eb307a37af255560bfcb057cdf
Alan Gutierrez <alan@prettyrobots.com>
2013-07-21 19:39:25 -0400

    Tidy increment example.
    It works again as a straight HTML application. It does not do
    `pushState` anymore.
    
    Closes #128.
    
    File                             Raw         Min         Zip     Min/Zip
    index.js                 31783/31.04 10408/10.16  7755/07.57  3943/03.85

--- executable.js
+++ executable.js
@@ -82,7 +82,7 @@ exports.routes = function routes (base) {
 
             delete match.params.pathInfo
 
-            stencils.generate(match.script, { greeting: "Hello, World!" }, step());
+            stencils.generate(match.script, { pathInfo: pathInfo }, step());
         }, function (generated) {
             response.setHeader("Content-Type", "text/html; charset=utf8");
             response.end(serializer(generated.document.documentElement));
