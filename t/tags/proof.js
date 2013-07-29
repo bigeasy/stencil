a0a8e1b43051765ec3c9401899c3130618c24cf1
Alan Gutierrez <alan@prettyrobots.com>
2013-07-29 00:47:34 -0400

    Add Stencil language to tags test harness.
    See #140.
    See #139.

--- t/tags/proof.js
+++ t/tags/proof.js
@@ -1,9 +1,12 @@
 var fs = require('fs'), path = require('path');
 module.exports = require('proof')(function () {
   var javascript = require('../../javascript/common').create(__dirname),
-      xml = require('../../xml/file').create(__dirname);
+      xml = require('../../xml/file').create(__dirname),
+      stencilParser = require('../../stencil').createParser(__dirname);
   var context =
   { context: require('../..').create(javascript, xml)
+  , xstencil: require('../..').create(javascript, xml)
+  , stencil: require('../..').create(javascript, stencilParser)
   , compare: require('../compare')
   , fixture: function (file, callback) { fs.readFile(path.resolve(__dirname, file), 'utf8', callback) }
   };
