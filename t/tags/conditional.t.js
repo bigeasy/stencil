155985989e3b83140893f20b0602ffb5696b1914
Alan Gutierrez <alan@prettyrobots.com>
2013-08-08 21:35:13 +0000

    Test tags in directives in Stencil language.
    Closes #165

--- t/tags/conditional.t.js
+++ t/tags/conditional.t.js
@@ -1,39 +1,66 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-require('./proof')(4, function (step, context, fixture, ok, compare) {
+require('./proof')(8, function (step, xstencil, stencil, fixture, ok, compare) {
 
   step(function () {
 
-    context.generate('fixtures/conditional.stencil', { greeting: "Hello, World!" }, step());
+    xstencil.generate('fixtures/conditional.xstencil', { greeting: "Hello, World!" }, step());
+    stencil.generate('fixtures/conditional.stencil', { greeting: "Hello, World!" }, step());
     fixture('fixtures/conditional-true.xml', step());
     fixture('fixtures/conditional-false.xml', step());
 
-  }, function (actual, truthy, falsey) {
+  }, function (xconditional, conditional, truthy, falsey) {
 
-    ok(compare(actual.document, truthy), 'true');
+    ok(compare(xconditional.document, truthy), 'xstencil true');
 
     step(function () {
 
-      context.regenerate(actual, {}, step());
+      xstencil.regenerate(xconditional, {}, step());
 
-    }, function (actual) {
+    }, function (xconditional) {
 
-      ok(compare(actual.document, falsey), 'false');
-      context.regenerate(actual, { greeting: "Hello, World!" }, step());
+      ok(compare(xconditional.document, falsey), 'xstencil false');
+      xstencil.regenerate(xconditional, { greeting: "Hello, World!" }, step());
 
-    }, function (actual) {
+    }, function (xconditional) {
 
-      ok(compare(actual.document, truthy), 'regenerating');
-      context.reconstitute(actual.document, step());
+      ok(compare(xconditional.document, truthy), 'xstencil regenerating');
+      xstencil.reconstitute(xconditional.document, step());
 
-    }, function (actual) {
+    }, function (xconditional) {
 
-      context.regenerate(actual, { greeting: "Hello, World!" }, step());
+      xstencil.regenerate(xconditional, { greeting: "Hello, World!" }, step());
 
-    }, function (actual) {
+    }, function (xconditional) {
 
-      ok(compare(actual.document, truthy), 'regenerating');
+      ok(compare(xconditional.document, truthy), 'xstencil regenerating');
+
+    });
+
+    ok(compare(conditional.document, truthy), 'stencil true');
+
+    step(function () {
+
+      stencil.regenerate(conditional, {}, step());
+
+    }, function (conditional) {
+
+      ok(compare(conditional.document, falsey), 'stencil false');
+      stencil.regenerate(conditional, { greeting: "Hello, World!" }, step());
+
+    }, function (conditional) {
+
+      ok(compare(conditional.document, truthy), 'stencil regenerating');
+      stencil.reconstitute(conditional.document, step());
+
+    }, function (conditional) {
+
+      stencil.regenerate(conditional, { greeting: "Hello, World!" }, step());
+
+    }, function (conditional) {
+
+      ok(compare(conditional.document, truthy), 'stencil regenerating');
 
     });
   });
