de1f99f6e28f524ef89eef1e4e5c03b6ed162936
Alan Gutierrez <alan@prettyrobots.com>
2013-08-13 00:20:02 +0000

    Implement sub tags in Stencil language.
    Closes #170.

--- t/tags/sub.t.js
+++ t/tags/sub.t.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-require('./proof')(3, function (step, xstencil, fixture, ok, compare) {
+require('./proof')(6, function (step, xstencil, stencil, fixture, ok, compare) {
+  var fs = require('fs');
 
   step(function () {
     xstencil.generate('fixtures/sub.xstencil', {}, step());
+    stencil.generate('fixtures/sub.stencil', {}, step());
     fixture('fixtures/sub.xml', step());
   },
 
-  function (xsub, expected) {
+  function (xsub, sub, expected) {
 
-    ok(compare(xsub.document, expected), 'generate');
+    ok(compare(xsub.document, expected), 'xstencil generate');
 
     step(function () {
       xstencil.regenerate(xsub, {}, step());
 
     }, function (xsub) {
 
-      ok(compare(xsub.document, expected), 'regenerate');
+      ok(compare(xsub.document, expected), 'xstencil regenerate');
       xstencil.reconstitute(xsub.document, step());
 
     }, function (xsub) {
@@ -26,7 +27,27 @@ require('./proof')(3, function (step, xstencil, fixture, ok, compare) {
 
     }, function (xsub) {
 
-      ok(compare(xsub.document, expected), 'reconstitute');
+      ok(compare(xsub.document, expected), 'xstencil reconstitute');
+
+    });
+
+    ok(compare(sub.document, expected), 'stencil generate');
+
+    step(function () {
+      stencil.regenerate(sub, {}, step());
+
+    }, function (sub) {
+
+      ok(compare(sub.document, expected), 'stencil regenerate');
+      stencil.reconstitute(sub.document, step());
+
+    }, function (sub) {
+
+      stencil.regenerate(sub, {}, step());
+
+    }, function (sub) {
+
+      ok(compare(sub.document, expected), 'stencil reconstitute');
 
     });
   });
